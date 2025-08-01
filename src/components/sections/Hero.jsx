b33b81ac02b8e9b16c55a343396a461e1fcf9230
Arteed <arteed19@gmail.com>
2025-08-01 12:48:02 +0800

    Refactoring the Hero Section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -4,7 +4,7 @@ import Chart from "../../assets/Chart.svg"
 
 function Hero (){
     return (
-        <section className="flex flex-col justify-center items-center bg-primary-darkBlue lg:flex-row text-center lg:text-left lg:h-[70vh] xl:gap-10">
+        <section className="flex flex-col justify-around items-center bg-primary-darkBlue lg:flex-row text-center lg:text-left lg:h-[50vh] xl:gap-10">
             <div className=" text-secondary-white lg:mx-5 xl:w-xl">
                 <h1 className="text-4xl font-bold mt-10 mb-5 lg:text-5xl xl:text-6xl">Get More Done with Whitespace</h1>
                 <p className="my-5 text-base font-light sm:font-normal md:px-5 lg:p-0 lg:text-xl xl:text-2xl">Project management software that enables your teams to collaborate, plan, analyze and manage every tasks</p>

94e4c7d733b93edbc4acddf39ab1fa556de2fac6
Raiden Fernando <157260780+raidenfernando1@users.noreply.github.com>
2025-08-18 21:53:50 +0800

    refactored -> components -> sections -> Sponsor.jsx

--- src/components/sections/Sponsor.jsx
+++ src/components/sections/Sponsor.jsx
@@ -3,31 +3,32 @@ import Microsoft from "../../assets/Microsoft.svg";
 import Slack from "../../assets/Slack.svg";
 import Google from "../../assets/Google.svg";
 
-function SponsorLogo ({Logo, logoName, imgSize = 'w-40 md:w-45 lg:w-55'}){
-  return (
-    <img src={Logo} className={imgSize} alt={logoName} />
-  );
+function SponsorLogo({ Logo, logoName, imgSize = "w-40 md:w-45 lg:w-55" }) {
+  return <img src={Logo} className={imgSize} alt={logoName} />;
 }
 
-
-
+const Sponsors = [
+  { Sponsor: "Apple", Logo: Apple, logoName: "Apple Logo" },
+  { Sponsor: "Microsoft", Logo: Microsoft, logoName: "Microsoft Logo" },
+  { Sponsor: "Slack", Logo: Slack, logoName: "Slack Logo" },
+  { Sponsor: "Google", Logo: Google, logoName: "Google Logo" },
+];
 
 function Sponsor() {
   return (
     <div className="w-full text-center">
       <div className="my-10">
-        <h1 className="text-4xl md:text-6xl font-bold text-secondary-black">
-          Our {" "}
+        <h1 className="text-secondary-black text-4xl font-bold md:text-6xl">
+          Our{" "}
           <span className="bg-[url(./assets/Element.svg)] bg-contain bg-bottom bg-no-repeat">
             Sponsors
           </span>
         </h1>
       </div>
-      <div className="flex flex-col items-center justify-around gap-5 sm:mx-5 sm:flex-row my-10">
-        <SponsorLogo Logo={Apple} logoName="Apple Logo"/>
-        <SponsorLogo Logo={Microsoft} logoName="Microsoft Logo"/>
-        <SponsorLogo Logo={Slack} logoName="Slack Logo"/>
-        <SponsorLogo Logo={Google} logoName="Google Logo"/>
+      <div className="my-10 flex flex-col items-center justify-around gap-5 sm:mx-5 sm:flex-row">
+        {Sponsors.map((Sponsor, Logo, logoName) => {
+          return <SponsorLogo key={Sponsor} Logo={Logo} logoName={logoName} />;
+        })}
       </div>
     </div>
   );
